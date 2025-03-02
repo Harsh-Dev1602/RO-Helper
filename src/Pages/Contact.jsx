5158a0a5cd4985366b654e59e85cc3ef5f02e459
Harsh Suryavanshi <harshsuryavanshi16022003@gmail.com>
2025-03-02 16:52:58 +0530

    RO-Helper-update-1.1.2.5

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -13,22 +13,22 @@ function Contact() {
         <p className="mt-4">We'd love to hear from you!</p>
       </header>
 
-      <section className="w-full mt-8 md:w-[50%] mx-auto flex justify-center items-center flex-col text-3xl gap-5 bg-gray-900 p-2 py-5 rounded-2xl rounded-tl-[50px] rounded-br-[50px]">
-        <h1 className=' shadow shadow-[#cccc] p-2 rounded-2xl flex font-bold gap-2'> <FaPhone className=' text-white text-4xl' />
-          <a href='tel:+919926052876' className=' text-blue-600 hover:underline' >+919926052876</a>
+      <section className="w-full mt-8 md:w-[50%] mx-auto flex justify-center items-center flex-col text-3xl gap-5 bg-gray-800 p-2 py-5 rounded-2xl rounded-tl-[50px] rounded-br-[50px]">
+        <h1 className=' bg-gray-900 shadow shadow-[#cccc] p-2 rounded-2xl flex  gap-2'> <FaPhone className=' text-white text-4xl' />
+          <a href='tel:+919926052876' className=' font-semibold text-2xl text-white hover:underline' >Contact Number</a>
         </h1>
 
-        <h1 className=' shadow shadow-[#cccc] p-2 rounded-2xl flex font-bold gap-2'><FaSquareWhatsapp className='text-white  text-4xl' />
-          <a href={`https://wa.me/${+919926052876}`} target="_blank" rel="noopener noreferrer" className=' text-blue-600 hover:underline' >+919926052876</a>
+        <h1 className=' bg-gray-900 shadow shadow-[#cccc] p-2 rounded-2xl flex  gap-2'><FaSquareWhatsapp className='text-white  text-4xl' />
+          <a href={`https://wa.me/${+919926052876}`} target="_blank" rel="noopener noreferrer" className=' font-semibold text-2xl text-white hover:underline' >Whatsapp</a>
         </h1>
 
-        <h1 className=' shadow shadow-[#cccc] p-2 rounded-2xl flex font-bold gap-2'> <FaFacebook className=' text-white text-4xl' />
-          <a target="_blank" rel="noopener noreferrer" href='https://www.facebook.com/OceanROService' className=' text-blue-600 hover:underline' >Facebook</a>
+        <h1 className=' bg-gray-900 shadow shadow-[#cccc] p-2 rounded-2xl flex  gap-2'> <FaFacebook className=' text-white text-4xl' />
+          <a target="_blank" rel="noopener noreferrer" href='https://www.facebook.com/OceanROService' className=' font-semibold text-2xl text-white hover:underline' >Facebook</a>
 
 
         </h1>
-        <h1 className=' shadow shadow-[#cccc] p-2 rounded-2xl flex font-bold gap-2'><FaInstagramSquare  className=' text-white text-4xl' />
-          <a target="_blank" rel="noopener noreferrer" href='https://www.instagram.com/ocean_water_solution?igsh=NG1xdnc4Ym43dTZr ' className=' text-blue-600 hover:underline' >Instagram</a>
+        <h1 className=' bg-gray-900 shadow shadow-[#cccc] p-2 rounded-2xl flex  gap-2'><FaInstagramSquare  className=' text-white text-4xl' />
+          <a target="_blank" rel="noopener noreferrer" href='https://www.instagram.com/ocean_water_solution?igsh=NG1xdnc4Ym43dTZr ' className=' font-semibold text-2xl text-white hover:underline' >Instagram</a>
 
           </h1>
       </section>
--- src/Pages/ContactForm.jsx
+++ src/Pages/ContactForm.jsx
@@ -50,27 +50,27 @@ function ContactForm() {
 
   return (
     <>
-      <main name="Contact Us" className=" h-auto  py-5 ">
+      <main name="Contact Us" className="w-[95%] mx-auto h-auto  py-5 ">
 
         <section className="mt-5">
           
           <form onSubmit={handleSubmit(onSubmit)}
-            className="space-y-4 mt-5  shadow rounded-2xl shadow-gray-500 p-5 mx-auto  md:w-[40%]">
+            className="space-y-4 mt-5 bg-gray-800 text-white  shadow rounded-2xl shadow-gray-500 p-5 mx-auto   md:w-[40%]">
             <div className="text-2xl font-semibold">Send Your Message</div>
             <div>
               <label htmlFor="name" className="block text-lg">Name</label>
-              <input name="name" {...register("name", { required: true })} type="text" id="name" placeholder=' Please enter a fullname..' className="w-full p-3 border border-gray-300 rounded-2xl  outline-none" />
-              {errors.name && <span className="text-red-600 pl-2.5">This field is required</span>}
+              <input name="name" {...register("name", { required: true })} type="text" id="name" placeholder=' Please enter a fullname..' className=" bg-white text-black w-full p-3 border border-gray-300 rounded-md  outline-none" />
+              {errors.name && <span className="text-red-500 pl-2.5">This field is required</span>}
             </div>
             <div>
               <label htmlFor="number" className="block text-lg"> Mobile Number</label>
-              <input name="number" {...register("number", { required: true })} type="number" placeholder=' Please enter a mobile number..' id="number" className="w-full p-3 border border-gray-300 rounded-2xl outline-none" />
-              {errors.number && <span className="text-red-600 pl-2.5">This field is required</span>}
+              <input name="number" {...register("number", { required: true })} type="number" placeholder=' Please enter a mobile number..' id="number" className="  bg-white text-black w-full p-3 border border-gray-300 rounded-md outline-none" />
+              {errors.number && <span className="text-red-500 pl-2.5">This field is required</span>}
             </div>
             <div>
               <label htmlFor="message" className="block text-lg">Message</label>
-              <textarea name="message" {...register("message", { required: true })} id="message" placeholder=' Enter your Query..' className="w-full min-h-30 p-3 border border-gray-300 rounded-2xl outline-none"></textarea>
-              {errors.message && <span className="text-red-600 pl-2.5">This field is required</span>}
+              <textarea name="message" {...register("message", { required: true })} id="message" placeholder=' Enter your Query..' className="  bg-white text-black  w-full min-h-30 p-3 border border-gray-300 rounded-md outline-none"></textarea>
+              {errors.message && <span className="text-red-500 pl-2.5">This field is required</span>}
             </div>
             <button type="submit" className="bg-blue-600 cursor-pointer hover:bg-blue-400 text-white p-3 rounded-2xl font-semibold">Send Message</button>
           </form>
