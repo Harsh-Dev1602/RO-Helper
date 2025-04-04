5e8062731f1162757b0ed719922383fcc02e1a74
Harsh Suryavanshi <Harsh-Dev1602@users.noreply.github.com>
2025-04-04 11:27:25 +0530

    up-to-date-2025.0.0.2.1

--- src/AddStructuredData.jsx
+++ src/AddStructuredData.jsx
@@ -5,12 +5,33 @@ const AddStructuredData = () => {
   return (
     <Helmet>
       <script type="application/ld+json">
-        {`{
-          "@context": "Ro Helper",
-          "@type": "WebSite",
-          "name": "Ro Helper  Call Us 9926052876",
-          "url": "https://www.rohelper.com"
-        }`}  
+        {`
+         {         
+            "@context": "https://schema.org/",         
+            "@type": "LocalBusiness",         
+            "@id": "#LocalBusiness",         
+            "url": "https://www.rohelper.com",          
+            "legalName": "Shambhulal  Suryavanshi",
+            "name": "RO Helper",
+            "description": "Looking for the best RO service in Indore We’ve got you covered! Our expert technicians provide top-quality RO repair, installation, and maintenance to ensure pure and safe drinking water.",
+            "image": "https://www.rohelper.com/assets/BG-IMG3-pdCVrVyF.png",
+            "logo": "https://www.rohelper.com/assets/LOGO-DBt3i4X8.png",
+            "telephone": "9926052876",
+            "faxNumber": "",
+            "email": "rohelper2025@gmail.com",
+            "address": {             
+              "@type": "PostalAddress",             
+              "streetAddress": "B-301 Shree Vinayak tower  satellite junction kelod hala",             
+              "addressLocality": "Indore",             
+              "addressRegion": "Madhya pradesh",             
+              "addressCountry": "India",             
+              "postalCode": "451020"        
+             }
+          }
+          } 
+        
+        `
+}  
       </script>
     </Helmet>
   );
