4ad855ce4f177198bf65742e27849ccc1a438f1b
Harsh Suryavanshi <Harsh-Dev1602@users.noreply.github.com>
2025-04-04 14:35:49 +0530

    up-to-date-2025.0.0.2.1

--- src/App.jsx
+++ src/App.jsx
@@ -5,30 +5,22 @@ import Contact from './Pages/Contact';
 import Footer from './Components/Footer';
 import Navbar from './Components/Navbar';
 import Icon from './Icon';
-import { BrowserRouter as Router,Routes,Route} from "react-router-dom";
 import { Toaster } from 'react-hot-toast';
 import { SpeedInsights } from '@vercel/speed-insights/react';
-import NotFound from "./Pages/NotFound";
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<>
-          <div>
-            <Navbar />
-            <Home />
-            <About />
-            <Contact />
-            <Footer />
-            <Icon />
-          </div>
-          <Toaster />
-          <SpeedInsights />
-        </>} />
-        {/* Catch-all route for 404 */}
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </Router>
+    <>
+      <div>
+        <Navbar />
+        <Home />
+        <About />
+        <Contact />
+        <Footer />
+        <Icon />
+      </div>
+      <Toaster />
+      <SpeedInsights />
+    </>
   );
 }
 
